130f1b7de4468d8c6bcc8be5d94ec45e30b7a309
Quan Xu <xuquan69220@gmail.com>
2022-10-13 15:34:56 +0200

    Add check horizontally function

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ for (const column of Array.from(document.getElementsByClassName("column"))) {
   grid.push(Array.from(column.children));
 }
 
+//Toggle player in Paragraph Element and put chips
 function handleClick(column) {
-  //Toggle player in Paragraph Element
-  //Put chips
-
   let Player = document.getElementById("player");
-
   const currentCircle = grid[column][bottomLine[column]];
 
   if (Player.innerText === "red") {
@@ -28,12 +25,42 @@ function handleClick(column) {
     currentCircle.classList.add("circleYellow");
   }
 
-  checkWinner();
+  checkWinner(currentCircle, column);
+
   bottomLine[column] = bottomLine[column] - 1;
 }
 
-function checkWinner() {
-  console.log("test");
+function checkWinner(currentCircle, column) {
+  const winnerArray = [currentCircle];
 
-  //window.alert("You are the Winner!!!");
+  // Check horizontally
+  let leftCircle;
+
+  for (let i = column - 1; i >= 0; i--) {
+    leftCircle = grid[i][bottomLine[column]];
+
+    if (leftCircle.className == currentCircle.className) {
+      winnerArray.unshift(leftCircle);
+    } else {
+      break;
+    }
+  }
+
+  let rightCircle;
+
+  for (let e = column + 1; e <= 5; e++) {
+    rightCircle = grid[e][bottomLine[column]];
+
+    if (rightCircle.className == currentCircle.className) {
+      winnerArray.push(rightCircle);
+    } else {
+      break;
+    }
+  }
+
+  if (winnerArray.length === 4) {
+    alert("Congratulations! You are the winner !!!");
+  }
+
+  // Check below currentCircle
 }
